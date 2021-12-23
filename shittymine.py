661f926091d3790919d406166b695348e2d3eeba
guifernand <gigas.tei@outlook.pt>
2021-12-23 01:40:11 +0000

    algumas instruçoes para saber o que fazer no futuro

--- shittymine.py
+++ shittymine.py
@@ -18,7 +18,7 @@ class square:
 
 #da update da matriz squares(representa o tabuleiro de jogo em valores numericos)
 def update(squares):
-    for l in range(1,max_lin+1):   #nao esquecer que para a matriz tera que ser l-1 para começar em 0, o mesmo para o c <--------------------
+    for l in range(1,max_lin+1):   #nao esquecer que para a matriz tera que ser l-1 para começar em 0, o mesmo para o c <-------------------- (matriz no site começa em 1 inves de 0)
         for c in range(1,max_col+1):
             id=str(l)+"_"+str(c)
             args=(By.ID, id)
@@ -27,11 +27,36 @@ def update(squares):
             if val_sq!="square blank":
                 squares[l-1][c-1].value=re.sub("\D", "", val_sq)     #retira apenas a parte numerica do argumento do square e mete na matriz (se demorar muito tempo podera ser possivel verificar se o valor novo e diferente do ja presente na matriz mas n sei se ajuda muito)
 
-#def maxProb(squares):
+
+#encontra na matriz os squares abertos que têm squares adjacentes por abrir que verifiquem self.value/(squares por if x!=l && y!=c && squares[x][y].value==-1 && squares[x][y].flag==False:
+#not_open_adj+=1;abrir adjacentes) =1 indicando que estes contem bomba e tem de ser flagged (self.flag=TRUE e bombs-=1)
+def maxProb(squares,l,c):  #(n sei se deve ser l ou l-1 e c ou c-1) porvavelmente necessita de uma flag para saber se encontrou algum com prob=1 porque senao abre se outro square random
+    x=l-1;                 #tem que se ter global bombas-=1
+    y=c-1;
+
+    toti_bomba=squares[l][c].value-squares[l][c].subtract; #é preciso subtract?!
+
+    not_open_adj=0;
+    while x<=(l+1):
+        while y<=(c+1):
+            if x!=l && y!=c && squares[x][y].value==-1 && squares[x][y].flag==False:
+                not_open_adj+=1;
+            c+=1
+        x+=1
+    #condicao para toti_bomb>0?
+    prob=not_open_adj/toti_bomba;
 
 
+#obter o numero de bombas ainda por detetar, ou seja, sera o value do square - o numero de flagged squares adjacentes
+def realvalue(squares):
 
-#variave iter indica se se trata da primiera iteracão (iter=1) ou nãp (iter=0)
+    for l in range(max_lin):
+        for c in range(max_col):
+            if squares[l][c].value!=-1 && squares[l][c].value!=0:
+                    while x<=(l+1):
+                        while y<=(c+1):
+
+#variave iter indica se se trata da primiera iteracão (iter=1) ou não (iter=0)
 def solver(iter):
 
     #selecionar primeiro square id="x_y"
@@ -49,30 +74,41 @@ def solver(iter):
             for C in range(0,max_col):
                 squares[L][C]=square()
 
+        #clicka no first
+        actions=ActionChains(driver)
+        actions.move_to_element(first).click()
+        actions.perform()
+
+        #update dos valores no squares abertos
+        update(squares)
+
     else:
         if condition:
+            #abrir o novo "first"
+            #update dos valores no squares abertos
+            #update(squares)
+            #realvalue(squares) (acho que e preciso caso a nova abertura seja junto de flagged squares)
             pass
         pass
-    #clicka no first
-    actions=ActionChains(driver)
-    actions.move_to_element(first).click()
-    actions.perform()
 
-    #update dos valores no squares abertos
-    update(squares)
+
 
     for L in range(0,max_lin):
         for C in range(0,max_col):
             print(squares[L][C].value)
+
     #enontrar um 1 num canto
     #dar flag nele (retirando ao valor do total de bombas) e retirar 1 a cada square adjacente (possivelmente necessario criar uma class para os squares com os seus valores que vao sendo atualizados e com o valor a reirar proveninete de bombas adjacentes)
     #repete se o processo varias vezes, se n encontrar um 1 num canto entao voltar a chamar a funcao solver (recusao) de forma a obter um novo first
 
     #while face!=facedead && bombas>=0:
 
-
-
-        #solver()
+        #maxProb
+        #abrir os com realvalue=0 && value!=0 para n tar a abrir squares no meio do nada
+        #update(squares)
+        #realvalue(squares)
+        #solver(0)
+        #face=
 
 #abrir o browser
 driver=webdriver.Firefox()
@@ -84,3 +120,12 @@ solver(1)
 
 
 #return element.get_attribute("class")
+
+#..............................................................
+
+# 1 passo- abrir o primeiro squares
+# 2 passo- update dos valores da grid
+# 3 passo- encontrar os squares com o maxProb=1
+# 4 passo- baseado no passo anterior dar flag aos devidos squares
+# 5 passo- fazer o realvalue dos squares
+# 6 passo- abrir os squares que ficam com 0 de realvalue
